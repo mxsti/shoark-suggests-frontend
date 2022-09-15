72ca059aee1965edcefc0137db848e9c550e1b3e
Lukas Traue <lukas.traue@deutschebahn.com>
2022-09-15 11:32:02 +0200

    refactoring

--- components/SuggestionCard.tsx
+++ components/SuggestionCard.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-
-type Suggestion = {
+type SuggestionProps = {
     thumbnail: string,
     name: string,
     streamingService: string,
@@ -8,39 +6,21 @@ type Suggestion = {
     rating: string,
 }
 
-const suggestionsDummy: Suggestion[] = 
-    [{
-        thumbnail: "https://m.media-amazon.com/images/M/MV5BMDZkYmVhNjMtNWU4MC00MDQxLWE3MjYtZGMzZWI1ZjhlOWJmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
-        name: "Stranger Things",
-        streamingService: "Netflix",
-        genre: "Drama, Fantasy, Horror",
-        rating: "8.7",
-    }]
-
-const SuggestionCard = () => {
-    const [suggestions, setSuggestions] = useState<Suggestion[]>(null);
-    useEffect(() => {
-      console.log(suggestions);
-      setSuggestions(suggestionsDummy);
-    });
-
-    return(
-      <div className="flex justify-center items-center h-screen">
-        {suggestions && suggestions.map((suggestion, index) => (
-          <div className="grid gap-2 grid-cols-4 suggestionCard">
-            <div className="col-span-2 p-2">
-              <img className="suggestionImg" src={suggestion.thumbnail} />
-            </div>
-            <div className="col-start-3 suggestionInfo">
-              <p className="py-2">{suggestion.name}</p>
-              <p className="py-2">{suggestion.rating}</p>
-              <p className="py-2">{suggestion.streamingService}</p>
-              <p className="py-2">{suggestion.genre}</p>
-            </div>
-          </div>
-        ))}
+const SuggestionCard = (props: SuggestionProps) => {
+  return(
+    <div className="suggestionCard">
+      <div className="col-span-2 p-2">
+        <img className="suggestionImg" src={props.thumbnail} />
       </div>
-    );
+      <div className="col-start-3 suggestionInfo">
+        <p className="py-2">{props.name}</p>
+        <p className="py-2">{props.rating}</p>
+        <p className="py-2">{props.streamingService}</p>
+        <p className="py-2">{props.genre}</p>
+      </div>
+    </div>
+  );
 }
+
 
 export default SuggestionCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
 import SuggestionCard from '../components/SuggestionCard'
 
+const suggestionsDummy = 
+  [{
+      thumbnail: "https://m.media-amazon.com/images/M/MV5BMDZkYmVhNjMtNWU4MC00MDQxLWE3MjYtZGMzZWI1ZjhlOWJmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+      name: "Stranger Things",
+      streamingService: "Netflix",
+      genre: "Drama, Fantasy, Horror",
+      rating: "8.7",
+  }]
+
 const Home: NextPage = () => {
+  const [suggestions, setSuggestions] = useState(suggestionsDummy);
+
   return (
     <div>
       <Head>
         <title>SHOARK SUGGESTS</title>
       </Head>
-      <SuggestionCard />
+      <div className="flex justify-center items-center h-screen">
+        {suggestions && suggestions.map((suggestion,index) => {
+          const suggestionIndentifier = `s-${index}`;
+          <div id={suggestionIndentifier}>
+            <SuggestionCard 
+              thumbnail={suggestion.thumbnail}
+              name={suggestion.name}
+              streamingService={suggestion.streamingService}
+              genre={suggestion.genre}
+              rating={suggestion.rating}
+            />
+          </div>
+        })}
+      </div>
     </div>
   )
 }

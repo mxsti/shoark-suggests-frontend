8ec92990b2184e75e034c178c52f720ce4ade32a
mxsty <lukastraue@gmail.com>
2022-09-28 22:54:21 +0200

    changed to api use

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,7 @@
 type Suggestion = {
-    id: number,
     imdbId: string,
     thumbnail: string,
     name: string,
-    streamingService: string,
     genre: string,
     rating: string,
     runtime: string,
